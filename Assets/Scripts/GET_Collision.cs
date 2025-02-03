f7aea95c2303d88691ef0a5cc46bd39246485b54
Whit <whitney.ellison@ou.edu>
2025-02-02 20:14:30 -0500

    Updated version

--- Assets/Scripts/GET_Collision.cs
+++ Assets/Scripts/GET_Collision.cs
@@ -6,40 +6,33 @@ public class GET_Collision : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         // If the object that collided with this object has the tag "Player"
-        if (other.CompareTag("TriggerObject"))
+        if (other.CompareTag("Player")) // Assuming "Player" is the tag of the player
         {
-            // Destroy the object that collided with this object
-            // Destroy(other.gameObject);
-            // print("Triggered Object Destroyed");
+            // Destroy this coin object when the player collides with it
+            Destroy(gameObject);
 
             // Show trigger feedback in Console Window
-            print("Trigger Entered");
+            print("Player picked up coin");
         }
     }
 
     void OnTriggerStay(Collider other)
     {
         // If the object that collided with this object has the tag "Player"
-        if (other.CompareTag("TriggerObject"))
+        if (other.CompareTag("Player")) // Assuming "Player" is the tag of the player
         {
-            // Destroy the object that collided with this object
-            // Destroy(other.gameObject);
-
-            // Show trigger feedback in Console Window
-            print("Trigger Stay");
+            // You can add additional logic here if needed for continuous triggers
+            print("Player counting coin");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
         // If the object that collided with this object has the tag "Player"
-        if (other.CompareTag("TriggerObject"))
+        if (other.CompareTag("Player")) // Assuming "Player" is the tag of the player
         {
-            // Destroy the object that collided with this object
-            Destroy(other.gameObject);
-
-            // Show trigger feedback in Console Window
-            print("Trigger Exit");
+            // You can add logic here if you want to do something when the player leaves the coin area
+            print("Player looking for more coins");
         }
     }
 }
